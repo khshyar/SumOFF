a3928639d8da4e8ff72e02250d78bc93837eaf25
Khashayar Behnam <khshyarg@gmail.com>
2024-04-22 00:00:48 +0200

    db new func

--- db/mysql_python_connector.py
+++ db/mysql_python_connector.py
@@ -2,30 +2,39 @@ import mysql.connector
 
 class SqlPy:
     def __init__(self):
-        self.conn = mysql.connector.connect(
-            host='localhost', username='root', password='SumOFF180424!', database='sumoff')
+        try:
+            self.conn = mysql.connector.connect(
+                host='localhost', username='root', password='SumOFF180424!', database='sumoff')
+            self.my_cursor = self.conn.cursor()
+        except Error as e:
+            print(f"Error connection to MySQL Platform: {e}")
 
+
+    def user_exists(self,email, username):
+        query = "SELECT * FROM user WHERE username = %s OR email = %s"
+        self.my_cursor.execute(query, (username,email))
+        result = self.my_cursor.fetchone()
+        return bool(result)
+        
 
 
     def push_db(self, email, username_data, password_data):
 
         try:
-            my_cursor = self.conn.cursor()
+            if self.user_exists(email, username_data):
+                print("User with this username or email already exists")
+                return    
 
-            my_cursor.execute(
-                "INSERT INTO user (`email`, `username`, `password`) VALUES (%s, %s, %s)",
-                (email, username_data, password_data)
-            )
-
+            query = "INSERT INTO user (`email`, `username`, `password`) VALUES (%s, %s, %s)"
+            self.my_cursor.execute(query,(email,username_data,password_data))
             self.conn.commit()
             print("User inserted successfully") 
         except mysql.connector.Error as err:
             print(f"Error: {err}")
         finally:
             # Close cursor
-            my_cursor.close()
-
-        print("Connection successfully created")
+            self.my_cursor.close()
+            self.my_cursor = self.conn.cursor()
     
     def close_connection(self):
         if self.conn:
