04deff278835cc0c14af8bc5497f47e45928567c
Khashayar Behnam <khshyarg@gmail.com>
2024-04-22 23:43:11 +0200

    add 2function

--- db/mysql_python_connector.py
+++ db/mysql_python_connector.py
@@ -1,32 +1,46 @@
 import mysql.connector
+from mysql.connector import Error
+
 
 class SqlPy:
-    def __init__(self):
+    def __init__(self, host='localhost', username='root', password='SumOFF180424!', database='sumoff'):
+        self.conn = None
+        self.my_cursor = None
         try:
-            self.conn = mysql.connector.connect(
-                host='localhost', username='root', password='SumOFF180424!', database='sumoff')
+            self.conn = mysql.connector.connect(host=host, username=username, password=password, database=database)
             self.my_cursor = self.conn.cursor()
         except Error as e:
-            print(f"Error connection to MySQL Platform: {e}")
+            print(f"Error connecting to MySQL Platform: {e}")
 
-
-    def user_exists(self,email, username):
-        query = "SELECT * FROM user WHERE username = %s OR email = %s"
-        self.my_cursor.execute(query, (username,email))
-        result = self.my_cursor.fetchone()
-        return bool(result)
-        
+    
+    def user_exists(self, username=None, email=None):
+        # Check if username exists
+        if username:
+            self.my_cursor.execute("SELECT 1 FROM user WHERE username = %s", (username,))
+            if self.my_cursor.fetchone():
+                return 'username'
+        # Check if email exists
+        if email:
+            self.my_cursor.execute("SELECT 1 FROM user WHERE email = %s", (email,))
+            if self.my_cursor.fetchone():
+                return 'email'
+        return None
+    
 
 
     def push_db(self, email, username_data, password_data):
-
+        
         try:
-            if self.user_exists(email, username_data):
-                print("User with this username or email already exists")
-                return    
-
-            query = "INSERT INTO user (`email`, `username`, `password`) VALUES (%s, %s, %s)"
-            self.my_cursor.execute(query,(email,username_data,password_data))
+            exists = self.user_exists(username=username, email=email)
+            if exists == 'username':
+                print("User with this username already exists")
+                return
+            elif exists == 'email':
+                print("User with this email already exists")
+                return
+        
+            query = "INSERT INTO user (email, username, password) VALUES (%s, %s, %s)"
+            self.my_cursor.execute(query, (email, username, password))
             self.conn.commit()
             print("User inserted successfully") 
         except mysql.connector.Error as err:
