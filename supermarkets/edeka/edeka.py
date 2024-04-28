6c9f1212d222b40f6fd56831c86960e187d152ec
Amin <mrx.tcx@gmail.com>
2024-04-29 03:27:00 +0330

    reading all products

--- supermarkets/edeka/edeka.py
+++ supermarkets/edeka/edeka.py
@@ -1,12 +1,53 @@
-from bs4 import BeautifulSoup
-import requests   
-header = {     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36' }  
-response = requests.get(url="https://www.edeka.de/eh/angebote.jsp", headers=header) 
-print(response) 
-web_html = response.text  
-soup = BeautifulSoup(web_html, "html.parser") 
-#print(soup) 
-base_category = soup.find("span", class_="css-1tty58m")  
-# items = soup.find(class_="css-79elbk") 
-# new_item = items.find(class_="css-1wq430v") 
-print(base_category)
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+URL = "https://www.edeka.de/eh/angebote.jsp"
+
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get(URL)
+
+# accept_cookies_btn = driver.find_element(By.ID, "popin_tc_privacy_button").click()
+
+accept_cookies_btn = WebDriverWait(driver, 30).until(
+    EC.element_to_be_clickable((By.ID, "popin_tc_privacy_button"))
+)
+accept_cookies_btn.click()
+
+WebDriverWait(driver, 60).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR,".css-6su6cu"))
+)
+
+products = driver.find_elements(By.CSS_SELECTOR, '.css-1uiiw0z')
+
+product_detail = []
+
+for product in products:
+    try:
+        price = product.find_element(By.CSS_SELECTOR, ".css-1tty58m").text
+        img = product.find_element(By.CSS_SELECTOR, "img").get_attribute("src")
+        try:
+            name = product.find_element(By.CSS_SELECTOR, ".css-6ha7pe").text
+        except Exception as e:
+            name = product.find_element(By.CSS_SELECTOR, ".css-ws0zws").text
+        product_detail.append({
+            "name": name,
+            "price": price,
+            "image": img
+        })
+    except Exception as e:
+        print("Couldnt extract a product", e)
+print(product_detail)
+print(len(product_detail))
+exit_driver = input("type 'exit' to quit the driver\n")
+
+if exit_driver == "exit":
+
+    driver.close()
+    driver.quit()
