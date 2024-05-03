37191002722a6dd58346e00e932de965a6b2341b
Khashayar Behnam <khshyarg@gmail.com>
2024-05-03 23:34:13 +0200

    Khashy

--- supermarkets/rewe/rewe_scraping.py
+++ supermarkets/rewe/rewe_scraping.py
@@ -2,6 +2,9 @@ from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 import time
 import csv
 
@@ -12,78 +15,98 @@ chrome_options.add_experimental_option("detach", True)
 # Initialisieren des Chrome Webdrivers
 driver = webdriver.Chrome(options=chrome_options)
 
-# Webseite laden
-driver.get("https://www.rewe.de/angebote/nationale-angebote/?week=current")
-time.sleep(5)  # Warten, damit die Webseite vollständig geladen wird
-
-# Extract the date range from the Tab Button
-date_range_element = driver.find_element(
-    By.CSS_SELECTOR, ".sos-week-tabs__tab-subtitle")
-date_range_text = date_range_element.text  # E.g., "29.4. bis 5.5."
-date_parts = date_range_text.split(' bis ')
-current_year = datetime.now().year
-
-# Correct parsing of the dates with the current year
-price_valid_from = datetime.strptime(
-    f"{date_parts[0]}{current_year}", "%d.%m.%Y").strftime("%d.%m.%Y")
-price_valid_until = datetime.strptime(
-    f"{date_parts[1]}{current_year}", "%d.%m.%Y").strftime("%d.%m.%Y")
-
-# Extraktion des Kategorienamens
-category_element = driver.find_element(
-    By.CSS_SELECTOR, "#sos-title-topangebote-current-week")
-# Hier wird der Kategorienname ausgelesen
-category_name = category_element.text.strip()
-
-# Suche nach dem übergeordneten Element, das alle Angebote enthält
-current_offers = driver.find_elements(
-    By.XPATH, '//div[@id="sos-category__grid-topangebote"]//article[@class="cor-offer-renderer-tile cor-link"]')
-
 # Dictionary, um alle Produkte und deren Preise zu speichern
 product_details = {}
 
-# Extraktion der Produktnamen und Preise
-for offer in current_offers:
-    product_name = offer.find_element(
-        By.CSS_SELECTOR, '.cor-offer-information__title-link').text.strip()
-    price = offer.find_element(
-        By.CSS_SELECTOR, '.cor-offer-price__tag-price').text.strip()
-    image_url = offer.find_element(
-        By.CSS_SELECTOR, '.tkr-lazy').get_attribute('src')
+try:
 
-    if product_name:  # Stelle sicher, dass der Produktname nicht leer ist
-        product_details[product_name] = {
-            'price': price,
-            'image_url': image_url,
-            'price_valid_from': price_valid_from,
-            'price_valid_until': price_valid_until,
-            'category': category_name  # Hinzufügen der gelesenen Kategorie
-        }
+    # Webseite laden
+    driver.get("https://www.rewe.de/angebote/nationale-angebote/?week=current")
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, '.sos-week-tabs__tab-subtitle')))
 
-current_date = datetime.now().strftime("%d-%m-%Y")
-filename = f"rewe_products_{current_date}.csv"
+    # shadow_host = driver.find_element(By.ID, "usercentrics-root")
+    # shadow_root = shadow_host.shadow_root
 
-csv_path = "E:/Coding/VSCode/SumOFF/csv_files/rewe"
+    # button = shadow_root.find_element(
+    #     By.CSS_SELECTOR, 'button[data-testid="uc-accept-all-button"]')
 
-# CSV-Datei schreiben
-with open(f"{csv_path}/{filename}", 'w', newline='', encoding='utf-8') as csvfile:
-    fieldnames = ['product_name', 'price', 'image_url',
-                  'price_valid_from', 'price_valid_until', 'category']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    for product, details in product_details.items():
-        writer.writerow({'product_name': product, 'price': details['price'], 'image_url': details['image_url'],
-                        'price_valid_from': details['price_valid_from'], 'price_valid_until': details['price_valid_until'], 'category': details['category']})
+    # # Click the button
+    # button.click()
+    # # Versuche, den Button mit JavaScript zu klicken
+    # accept_cookies_btn = WebDriverWait(driver, 15).until(
+    #     EC.presence_of_element_located((By.CSS_SELECTOR, '[data-testid="uc-accept-all-button"]')))
+    # driver.execute_script("arguments[0].click();", accept_cookies_btn)
+
+    # Extract the date range from the Tab Button
+    date_range_element = driver.find_element(
+        By.CSS_SELECTOR, ".sos-week-tabs__tab-subtitle")
+    date_range_text = date_range_element.text  # E.g., "29.4. bis 5.5."
+    date_parts = date_range_text.split(' bis ')
+    current_year = datetime.now().year
+
+    # Correct parsing of the dates with the current year
+    price_valid_from = datetime.strptime(
+        f"{date_parts[0]}{current_year}", "%d.%m.%Y").strftime("%d.%m.%Y")
+    price_valid_until = datetime.strptime(
+        f"{date_parts[1]}{current_year}", "%d.%m.%Y").strftime("%d.%m.%Y")
+
+    # Extraktion des Kategorienamens
+    category_element = driver.find_element(
+        By.CSS_SELECTOR, "#sos-title-topangebote-current-week")
+    # Hier wird der Kategorienname ausgelesen
+    category_name = category_element.text.strip()
+
+    # Suche nach dem übergeordneten Element, das alle Angebote enthält
+    current_offers = driver.find_elements(
+        By.XPATH, '//div[@id="sos-category__grid-topangebote"]//article[@class="cor-offer-renderer-tile cor-link"]')
+
+    # Extraktion der Produktnamen und Preise
+    for offer in current_offers:
+        product_name = offer.find_element(
+            By.CSS_SELECTOR, '.cor-offer-information__title-link').text.strip()
+        price = offer.find_element(
+            By.CSS_SELECTOR, '.cor-offer-price__tag-price').text.strip()
+        image_url = offer.find_element(
+            By.CSS_SELECTOR, '.tkr-lazy').get_attribute('src')
+
+        if product_name:  # Stelle sicher, dass der Produktname nicht leer ist
+            product_details[product_name] = {
+                'price': price,
+                'image_url': image_url,
+                'price_valid_from': price_valid_from,
+                'price_valid_until': price_valid_until,
+                'category': category_name  # Hinzufügen der gelesenen Kategorie
+            }
+
+    current_date = datetime.now().strftime("%d-%m-%Y")
+    filename = f"rewe_products_{current_date}.csv"
+
+    csv_path = "E:/Coding/VSCode/SumOFF/csv_files/rewe"
+
+    # CSV-Datei schreiben
+    with open(f"{csv_path}/{filename}", 'w', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['product_name', 'price', 'image_url',
+                      'price_valid_from', 'price_valid_until', 'category']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for product, details in product_details.items():
+            writer.writerow({'product_name': product, 'price': details['price'], 'image_url': details['image_url'],
+                            'price_valid_from': details['price_valid_from'], 'price_valid_until': details['price_valid_until'], 'category': details['category']})
+
+except Exception as e:
+    print(f"Ein Fehler ist aufgetreten: {e}")
+    driver.quit()
+
+finally:
+    # Warten auf Benutzereingabe, um das Programm zu beenden
+    # input("Press Enter to quit...")
+
+    # Schließen des Browsers
+    driver.quit()
 
 # Ausgabe des Dictionaries mit Produktnamen und Preisen
 print(product_details)
-
-# Warten auf Benutzereingabe, um das Programm zu beenden
-input("Press Enter to quit...")
-
-# Schließen des Browsers
-driver.quit()
-# driver.close()
 
 
 # from bs4 import BeautifulSoup
